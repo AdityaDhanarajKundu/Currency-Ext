7670a740cd67942f2a6f14cc3af7bd622eaa9d33
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-08-03 20:50:25 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,14 @@
+const amountEl = document.getElementById("amount");
+const haveEl = document.getElementById("have");
+const wantEl = document.getElementById("want");
+const convertEl = document.getElementById("convert");
+const resultEl = document.getElementById("result");
+
+const api_key="";
+const api_url=``;
+
+//adding function to the button click
+convertEl.addEventListener("click",()=>{
+    let amountTotal = amountEl.value; //to get the value present in the amountEl
+    let currencyTotal = currencyEl.value;
+});
