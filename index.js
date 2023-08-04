4c3e45ae2cab9621b50f4ce4c8943bc8114ca18b
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-08-04 07:57:48 +0530

    corrected the bugs and completed the js code

--- index.js
+++ index.js
@@ -4,29 +4,28 @@ const wantEl = document.getElementById("want");
 const convertEl = document.getElementById("convert");
 const resultEl = document.getElementById("result");
 
-const api_key="RJ+rtM6A+dytAHo6DfcA0Q==L6oUfNfUrX3dlqCp";
+const api_key = "RJ+rtM6A+dytAHo6DfcA0Q==L6oUfNfUrX3dlqCp";
 
-//adding function to the button click
-convertEl.addEventListener("click",()=>{
-    let amountTotal = amountEl.value; //to get the value present in the amountEl
+// Adding function to the button click
+convertEl.addEventListener("click", () => {
+    let amountTotal = amountEl.value;
     let haveTotal = haveEl.value;
     let wantTotal = wantEl.value;
     let api_url = `https://api.api-ninjas.com/v1/convertcurrency?want=${wantTotal}&have=${haveTotal}&amount=${amountTotal}`;
 
-    fetch(api_url,{
+    $.ajax({
         method: 'GET',
-        headers:{
-            'X-Api-Key': api_key
+        url: api_url, // Use the api_url variable here
+        headers: { 'X-Api-Key': api_key }, // Use the api_key variable here
+        contentType: 'application/json',
+        success: function(result) {
+            resultEl.textContent="";
+            console.log(result);
+            resultEl.textContent=`${result.old_amount} ${result.old_currency} = ${result.new_amount} ${result.new_currency}`;
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+            resultEl.textContent="Can't get the exchange rates, please try again";
         }
-    })
-    .then(response=>response.json())
-    .then(data=>{
-        const rate = data.exchange_rate;
-        const result = amountTotal*rate;
-        resultEl.innerHTML=`${amountTotal} ${haveTotal} = ${result.toFixed(2)} ${wantTotal}`;
-    })
-    .catch(error=>{
-        console.error("Request Failed, ",error);
-        resultEl.textContent="An error occured, please try again";
-    })
-});
+    });
+}); // Remove the extra parenthesis here
